d2c82bde7dc598df6768af97a81578f07dc334ed
NELSON ANDRES LUENGAS SILVA <90053867+NELSONLUENGAS@users.noreply.github.com>
2024-08-15 18:39:29 -0500

    Grid layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Products from './views/Products';
 
 function App() {
 	return (
 		<>
 			<Header />
+			<Products />
 			<Footer />
 		</>
 	);
---
+++ src/views/Products.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Products = (props) => {
+	return (
+		<>
+			<div className="container">
+				<div className="grid-products"></div>
+			</div>
+		</>
+	);
+};
+
+Products.propTypes = {};
+
+export default Products;
